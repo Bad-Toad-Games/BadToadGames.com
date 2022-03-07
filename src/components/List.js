ba15774b658783d35500b3b810c5fe3ada004985
Pignuuu <Pignuuu@gmail.com>
2022-03-07 16:17:56 +0100

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -5,10 +5,10 @@ import { NavLink } from "react-router-dom";
 function List() {
     return (
         <div className={List_Style.Main}>
-            <NavLink to="/Merch" className={List_Style.Button}>MERCH</NavLink>
-            <NavLink to="/Tabletop" className={List_Style.Button}>TABLETOP<br />GAMES</NavLink>
-            <NavLink to="/Video-Games" className={List_Style.Button}>VIDEO GAMES</NavLink>
-            <NavLink to="/Projects" className={List_Style.Button}>PROJECTS</NavLink>
+            <NavLink to="/merch" className={List_Style.Button}>MERCH</NavLink>
+            <NavLink to="/tabletop-games" className={List_Style.Button}>TABLETOP<br />GAMES</NavLink>
+            <NavLink to="/games" className={List_Style.Button}>VIDEO GAMES</NavLink>
+            <NavLink to="/projects" className={List_Style.Button}>PROJECTS</NavLink>
         </div >
     );
 }
