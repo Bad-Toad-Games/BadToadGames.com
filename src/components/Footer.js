a73a369467062a1bfc1ea8bb655c0cf14d79c4de
Pignuuu <Pignuuu@gmail.com>
2022-03-08 19:15:40 +0100

    Remove _blank

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,13 +10,13 @@ function Header() {
     return (
         <div className={Footer_Style.Main}>
             <div className={Footer_Style.Icons}>
-                <a href="https://www.youtube.com/channel/UC9vWLWdzsHvUDAR1EjziV4w" target="_blank" className={Footer_Style.Icon_href}>
+                <a href="https://www.youtube.com/channel/UC9vWLWdzsHvUDAR1EjziV4w" className={Footer_Style.Icon_href}>
                     <FaYoutube className={Footer_Style.Icon} />
                 </a>
-                <a href="https://twitter.com/bad_toad_games" target="_blank" className={Footer_Style.Icon_href}>
+                <a href="https://twitter.com/bad_toad_games" className={Footer_Style.Icon_href}>
                     <FaTwitter className={Footer_Style.Icon} />
                 </a>
-                <a href="https://discord.gg/3SqXXEaSCn" target="_blank" className={Footer_Style.Icon_href}>
+                <a href="https://discord.gg/3SqXXEaSCn" className={Footer_Style.Icon_href}>
                     <SiDiscord className={Footer_Style.Icon} />
                 </a>
             </div>
