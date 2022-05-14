58fab7c61fc8dadc7069ecce97529f84a1152217
Pignuuu <Pignuuu@gmail.com>
2022-05-14 11:30:16 +0200

    Restart project in Vue.JS

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./navbar/Navbar";
-export { default as List } from "./List";
-export { default as Header } from "./Header";
-export { default as Footer } from "./Footer";
